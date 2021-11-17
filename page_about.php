716437c0ac27047b729f346bc277004ebd044db8
Jacob Geib-Rosch <jake@xplusd.com>
2021-11-17 18:55:11 -0500

    Undoing Test #2
    Second test no good. Onto #3.

--- page_about.php
+++ page_about.php
@@ -12,13 +12,5 @@
  * @link    https://my.studiopress.com/themes/monochrome/
  */
 
-
-add_action( 'genesis_before', function() {
-
-	// Add your custom code here.
-	<p>test</p>
-
-});
-
 // Run the Genesis loop.
 genesis();
