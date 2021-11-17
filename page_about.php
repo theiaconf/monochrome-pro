3f72fab6356fe1210dabe2e45febeb9882644458
Jacob Geib-Rosch <jake@xplusd.com>
2021-11-17 18:47:23 -0500

    Removing Genesis Test #1
    First attempt failed. Reverting.

--- page_about.php
+++ page_about.php
@@ -12,15 +12,7 @@
  * @link    https://my.studiopress.com/themes/monochrome/
  */
 
-<?php
 
-add_action( 'genesis_before_entry_content', 'theme_prefix_show_notice' );
-/**
- * Display a custom notice.
- */
-function theme_prefix_show_notice() {
-	echo '<p class="notice">This page has a custom template.</p>';
-}
 
 // Run the Genesis loop.
 genesis();
