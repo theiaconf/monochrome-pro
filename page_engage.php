58edcdabf92c437f6a43523367060973206e1bf2
Jacob Geib-Rosch <jake@xplusd.com>
2021-11-17 20:54:59 -0500

    Adding Banner HTML
    Adding proof-of-concept banner HTML after a successful test of Genesis HTML injection on Engage template.

--- page_engage.php
+++ page_engage.php
@@ -17,7 +17,29 @@ add_action( 'genesis_before_entry_content', 'theme_prefix_show_notice' );
  * Display a custom notice.
  */
 function theme_prefix_show_notice() {
-	echo '<p class="notice">This page has a custom template.</p>';
+	echo '
+		
+		<div class="front-page-1 image-section">
+			<div class="flexible-widgets widget-area fadeup-effect widget-full fadeInUp">
+				<div class="wrap">
+					<section id="textblockswidget-14" class="widget widget_textblockswidget">
+						<div class="widget-wrap">
+							<div class="text-block hometown-hero">
+								<p>Join us online for<br>
+								IAC: the information architecture conference</p>
+								<h2 class="hero-title hero-theme">[Re]Connect</h2>
+								<p>April 19th-23rd, 2022</p>
+								<div class="secondary_cta">
+									Registration opens in December
+								</div>
+							</div>
+						</div>
+					</section>
+		        </div>
+			</div>
+		</div>
+		
+		';
 }
 
 // Run the Genesis loop.
