1bdab3284107e1f6646fe5317d619dfbf16abbb7
Jacob Geib-Rosch <jake@xplusd.com>
2021-12-14 11:12:08 -0500

    Banner Title Updates
    Update to titles on banners to match latest comp/sitemap.

---
+++ page_attendee-login.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Monochrome Pro.
+ *
+ * Template Name: Attendee Login
+ *
+ * This file supports design elements linked to this template.
+ *
+ * @package Monochrome
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    https://my.studiopress.com/themes/monochrome/
+ */
+
+add_action( 'genesis_after_header', 'theme_prefix_show_notice' );
+function theme_prefix_show_notice() {
+	echo '
+		
+		<div class="masthead-banner">
+			<div class="text-block">
+				<span class="banner-site-title">IAC 22</span>
+				<span class="banner-site-subtitle"><strong>[Re]</strong>Connect</span>
+				<span class="banner-section-title"></span>
+			</div>
+		</div>
+		
+		';
+}
+
+// Run the Genesis loop.
+genesis();
