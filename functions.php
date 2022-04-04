ed25911161ab2517b1562c3e1b6eac74373ccd03
Nathan Rogers <nrogers@unithq.com>
2022-04-03 23:29:53 -0500

    Allow shortcodes in PODS Templates

--- functions.php
+++ functions.php
@@ -881,3 +881,16 @@ function change_sender_name($original_email_from) {
     return 'The IA conference';
 }
 add_filter('wp_mail_from_name','change_sender_name');
+
+/**
+ * Allow Pods Templates to use shortcodes
+ *
+ * NOTE: Will only work if the constant PODS_SHORTCODE_ALLOW_SUB_SHORTCODES is
+ * defined and set to true, which by default it IS NOT.
+ */
+add_filter( 'pods_shortcode', function( $tags )  {
+  $tags[ 'shortcodes' ] = true;
+  
+  return $tags;
+  
+});
