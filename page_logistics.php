b9e3b1b1c8e30e31d5a660d9079fe022592f498c
Nathan Rogers <narogers@users.noreply.github.com>
2023-12-07 17:01:33 -0600

    Base theme restore (#43)
    * before-footer-cta background color to almost black instead of blue gradient
    * add style-front.css back with gradient and large header
    * front page button styling
    * front page hero styling
    * co chair image and text formatting
    * add fonts folder and raptor font
    * raptor font on front page h1
    * center the frontpage button
    * font face reference to raptor name
    * Add image replacement code for logo
    * Update font-face declarations
    * Custom styling for program and workshops
    * Style additions for audience levels
    * Add new template for Workshops
    * Add custom style for workshop theming
    * Add new template for logistics about IAC24
    * Add background images for theme
    * Add custom styling for page header to be yellow
    * Add basic background image for page heading
    * Make margins on pages automatic
    * Add fix for site logo hyperlink
    * Use new IAC24 logo
    * Left align front page content
    * Add custom styling for IAC24 informational elements
    * Adjustments for co-chair information
    * Adjustments for the text in the page footer
    * Tweaks for the footer including background images
    * Fix styles for call to action buttons
    * Adjust styling for the home page calls to action
    * Sensible defaults for headings
    
    ---------
    
    Co-authored-by: VA Corvid <virginia@virginiacorvid.design>
    Co-authored-by: Nathan Rogers <nrogers@unithq.com>

--- page_logistics.php
+++ page_logistics.php
@@ -2,7 +2,7 @@
 /**
  * Monochrome Pro.
  *
- * Template Name: Program
+ * Template Name: Logistics
  *
  * This file supports design elements linked to this template.
  *
@@ -16,14 +16,21 @@ add_action( 'genesis_after_header', 'theme_prefix_show_notice' );
 function theme_prefix_show_notice() {
 	echo '
 		
-		<div class="masthead-banner">
-			<div class="text-block">
-				<span class="banner-site-title">Workshops</span>
-			</div>
+		<div class="masthead-banner logistics">
+			<div class="section-title">Logistics</div>
 		</div>
 		
 		';
 }
+
+function custom_body_classes($classes) {
+  $classes[] = 'logistics';
+  return $classes;
+}
+
+add_action('body_class', 'custom_body_classes');
+
+remove_action('genesis_entry_header', 'genesis_do_post_title');
 
 // Run the Genesis loop.
 genesis();
