026cf3af9f31cc5f46fae57bfe3a83110d4bfaa8
Nathan Rogers <narogers@users.noreply.github.com>
2024-01-16 12:32:02 -0600

    Program pages (#44)
    * Add jump link styles for program pages
    * Rule specificity for session titles
    * Add custom styles for the session detail pages
    * Add custom styles for the speaker detail pages
    * Add a new style for cochairs to match presenters
    * Make default hover state for buttons black
    * Add scroll margin for jump links
    * Add margin to bottom of tickeroter
    * Adjust alignment of header and logo in footer
    * Fix registration links
    * Add styling for posters to match workshops and sessions
    
    ---------
    Co-authored-by: Nathan Rogers <nrogers@unithq.com>

--- page_program.php
+++ page_program.php
@@ -2,7 +2,7 @@
 /**
  * Monochrome Pro.
  *
- * Template Name: Program
+ * Template Name: Workshops
  *
  * This file supports design elements linked to this template.
  *
@@ -16,14 +16,33 @@ add_action( 'genesis_after_header', 'theme_prefix_show_notice' );
 function theme_prefix_show_notice() {
 	echo '
 		
-		<div class="masthead-banner">
-			<div class="text-block">
-				<span class="banner-site-title">Workshops</span>
-			</div>
-		</div>
-		
+		<div class="masthead-banner program">
+		  <div class="text-block">
+			<div class="section-title">Program</div>
+		  </div>
+		</div>	
+	   <div class="cta-ticker">
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	     <a href="/iac24-registration">Register &gt; </a>
+	   </div>
 		';
 }
+
+function custom_body_classes($classes) {
+  $classes[] = 'program';
+  return $classes;
+}
+
+add_action('body_class', 'custom_body_classes');
+remove_action('genesis_entry_header', 'genesis_do_post_title');
 
 // Run the Genesis loop.
 genesis();
